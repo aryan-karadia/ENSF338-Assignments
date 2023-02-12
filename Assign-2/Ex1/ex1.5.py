9ecaa4957a917d58c508bcd50028fd12210a83ea
Aryan Karadia <karadiaaryan@gmail.com>
2023-02-12 15:49:36 -0700

    ex plot

--- Assign-2/Ex1/ex1.5.py
+++ Assign-2/Ex1/ex1.5.py
@@ -1,24 +1,33 @@
 import timeit
 import matplotlib.pyplot as plt
 
-def func2(n, memo={}):
-    if n == 0 or n == 1:
-        return 1
-    if n in memo:
-        return memo[n]
-    else:
-        memo[n] = func2(n-1, memo) + func2(n-2, memo)
-        return memo[n]
-
 def func(n):
     if n == 0 or n == 1:
         return 1
     return func(n-1) + func(n-2)
 
-outputMemo = [timeit.timeit(lambda: func2(i)) for i in range(0, 36)]
+def func1(n, memo={}):
+    if n == 0 or n == 1:
+        return 1
+    if n in memo:
+        return memo[n]
+    else:
+        memo[n] = func1(n-1, memo) + func1(n-2, memo)
+        return memo[n]
 
-output = [timeit.timeit(lambda: func(i)) for i in range(0, 36)]
+originoutput = []
 
-plt.plot(outputMemo, label="Memoization", color="red")
-plt.plot(output, label="Original", color="blue")
+for i in range(0, 36):
+    originoutput.append(timeit.timeit("func(i)", setup="from __main__ import func, i", number=1))
+
+memooutput = []
+
+for i in range(0, 36):
+    memooutput.append(timeit.timeit("func1(i)", setup="from __main__ import func1, i", number=1))
+
+plt.plot(originoutput, label="origin")
+plt.plot(memooutput, label="memo")
+plt.xlabel("nth Fib Number")
+plt.ylabel("Time")
+plt.legend()
 plt.show()
