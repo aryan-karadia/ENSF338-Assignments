778e44aa1b99b871c1663b055f40359676f3a38f
AdityaP16 <ap.prasad@outlook.com>
2023-02-12 16:50:35 -0700

    ex2.2.py finished

--- Assign-2/Ex2/ex2.2.py
+++ Assign-2/Ex2/ex2.2.py
@@ -1,6 +1,6 @@
-## Code given to us
-
 import sys
+import time
+import matplotlib.pyplot as plt
 sys.setrecursionlimit(20000)
 
 def func1(arr, low, high):
@@ -24,3 +24,28 @@ def func2(array, start, end):
             break
     array[start], array[high] = array[high], array[start]
     return high
+
+# Import the .json file
+import json
+with open('Ex2/ex2.json') as f:
+    arr = json.load(f)
+
+# Measure the time taken to sort the array using func1
+start_time = time.time()
+func1(arr, 0, len(arr) - 1)
+time_taken_func1 = time.time() - start_time
+
+# Measure the time taken to sort the array using func2
+start_time = time.time()
+func2(arr, 0, len(arr) - 1)
+time_taken_func2 = time.time() - start_time
+
+# Plot the time taken to sort the array using both functions
+x = [1, 2]
+y = [time_taken_func1, time_taken_func2]
+labels = ['func1', 'func2']
+plt.bar(x, y, tick_label=labels)
+plt.xlabel('Function')
+plt.ylabel('Time Taken (Seconds)')
+plt.title('Time Comparison of func1 and func2')
+plt.show()
